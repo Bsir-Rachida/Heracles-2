a34d5427b7d6e6dda892bcc2534d7c283db2f8a9
Bsir-Rachida <90473644+Bsir-Rachida@users.noreply.github.com>
2021-10-18 19:30:47 +0200

    Add files via upload

---
+++ Weapon.php
@@ -0,0 +1,22 @@
+<?php
+
+class Weapon{
+ private int $damage = 10;
+ private string $image = 'sword.svg';
+
+
+ public function getDamage(): int{
+    return $this->damage;
+ }
+
+ public function setDamage(int $damage):void{
+    $this->damage= $damage;
+ }
+ public function getImage():string{
+    return 'assets/images/' . $this->image;
+ }
+
+ public function setImage(string $damage):void{
+    $this->image= $image;
+ }
+}
